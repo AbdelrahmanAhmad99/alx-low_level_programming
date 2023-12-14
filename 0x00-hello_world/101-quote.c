718c7525f44cb5f3d02cb9fe56b0f4ee191fff8b
abdelrahman <abdelrahmanahmad78780@gmail.com>
2023-12-14 22:54:52 +0200

    The project is 0x03-shell_variables_expansions please done.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ *main - main point go go
+ *
+ *
+ *bad boy go away
+ *
+ *Return:1
+*/
+int main(void)
+{
+char dodo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, dodo, 59);
+return (1);
+}
