d629b3df50706f8bd29c29d505ad5bbb0b7a5ea1
abdelrahman <abdelrahmanahmad78780@gmail.com>
2023-12-14 21:44:03 +0200

    The project is 0x03-shell_variables_expansions please done .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main wow 
+ *
+ * go go go
+ *
+ * Return:sss 0 (aa)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
